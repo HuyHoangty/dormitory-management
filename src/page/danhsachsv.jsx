72b68c0b9e3b53d1e3da944bb4c7e12b780dfe09
huyhoangtrinh <ttrinhhuyhoangg@gmail.com>
2024-12-04 11:29:10 +0700

    Add files via upload

---
+++ src/page/danhsachsv.jsx
@@ -0,0 +1,267 @@
+import React from 'react';
+
+const danhsachsv = () => {
+    return (
+        <div>
+            <header className="text-white ">
+                <div className="logo"><img src="../../public/image/icon/logo.png" alt /><span className style={{ width: '100%' }}>KÝ TÚC
+                    XÁ</span></div>
+                <div className="menu ">
+                    <nav className="navbar navbar-expand-lg navbar-light ">
+                        <div className="container-fluid">
+                            <a className="navbar-brand" href="#">
+                            </a>
+                            <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon" />
+                            </button>
+                            <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                                <div className="navbar-nav" style={{ fontWeight: 500 }}>
+                                    <a className="nav-link active text-white" aria-current="page" href="#"><i className="bi bi-telephone-forward-fill p-1 fs-5" />Điện thoại hỗ trợ: 0333444555</a>
+                                    <a className="nav-link text-white" href><img src="../../public/image/bg-login.jfif" alt /></a>
+                                    <a className="nav-link text-white" href><i className="fa-solid fa-power-off" /></a>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <div className="row">
+                <div className="col-lg-3 bg-menu">
+                    <nav className="menu">
+                        <ul className="list-unstyled">
+                            <li className="parent-menu position-relative on">
+                                <i className="bi bi-door-open" />
+                                Quản lý phòng
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-border-style" />
+                                        Sơ đồ phòng
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-patch-plus" />
+                                        Thêm phòng
+                                    </li>
+                                </a>
+                            </ul>
+                            <li className="parent-menu position-relative on">
+                                <i className="bi bi-person-walking" />
+                                Quản lý sinh viên
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-border-style" />
+                                        Danh sách sinh viên
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-patch-plus" />
+                                        Thêm sinh viên
+                                    </li>
+                                </a>
+                            </ul>
+                            <li className="parent-menu position-relative on">
+                                <i className="fas fa-male" />
+                                Quản lý nhân viên
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-border-style" />
+                                        Danh sách quản lý
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="fa-solid fa-plus" />
+                                        Thêm quản lý
+                                    </li>
+                                </a>
+                            </ul>
+                            <li className="parent-menu position-relative on">
+                                <i className="bi bi-graph-up-arrow" />
+                                Báo cáo thống kê
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-door-closed-fill" />
+                                        Thống kê phòng
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-person-walking" />
+                                        Thống kê sinh viên
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-calendar-month-fill" />
+                                        Thống kê hoá đơn
+                                    </li>
+                                </a>
+                            </ul>
+                            <li className="parent-menu position-relative on">
+                                <i className="fas fa-user-shield" />
+                                Quản trị viên
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-link" />
+                                        Liên kết
+                                    </li>
+                                </a>
+                            </ul>
+                            <li className="parent-menu position-relative on">
+                                <i className="bi bi-person-circle" />
+                                Quản lý tài khoản
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-person-lines-fill" />
+                                        Danh sách tài khoản
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-person-plus-fill" />
+                                        Tạo tài khoản
+                                    </li>
+                                </a>
+                            </ul>
+                            <li className="parent-menu position-relative on">
+                                <i className="bi bi-receipt" />
+                                Quản lý hoá đơn
+                                <span className="arrow position-absolute">
+                                    <i className="bi bi-chevron-left left d-none" />
+                                    <i className="bi bi-chevron-down down" />
+                                </span>
+                            </li>
+                            <ul className="list-unstyled sub-menu">
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-plus-circle-fill" />
+                                        Xuất hoá đơn
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-droplet-fill" />
+                                        Hoá đơn điện nước
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-house-door-fill" />
+                                        Hoá đơn phòng
+                                    </li>
+                                </a>
+                                <a href>
+                                    <li>
+                                        <i className="bi bi-bicycle" />
+                                        Hoá đơn gửi xe
+                                    </li>
+                                </a>
+                            </ul>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="col-lg-9 sdp tp" style={{ minHeight: 1000 }}>
+                    <div className="box">
+                        <div className="name">
+                            <i className="bi bi-people-fill" />Sinh viên
+                        </div>
+                        <div className="find">
+                            <div className="find-tang">
+                                <div className="fs-15 fw-500">Tầng:</div>
+                                <select className="form-select " aria-label="Default select example" id="fillter_t">
+                                    <option value={0}>Tất cả</option>
+                                    <option value={1}>Tầng 1</option>
+                                    <option value={2}>Tầng 2</option>
+                                    <option value={3}>Tầng 3</option>
+                                </select>
+                            </div>
+                            <div className="ttp">
+                                <div className="fs-15 fw-500">Trạng thái:</div>
+                                <nav className="navbar navbar-light">
+                                    <form action method="get" className="container-fluid justify-content-start pd-0" id="form_fillter">
+                                        <a href="./danhsachsv.html?tt=0" className="mgr-10"><button className="btn me-2" type="button" id="tatca">Tất cả</button></a>
+                                        <a href="./danhsachsv.html?tt=2"><button className="btn me-2" type="button" id="dango"><img src="../../public/image/icon/audience.png" alt className="img-icon" />Đang
+                                            ở</button></a>
+                                        <a href="./danhsachsv.html?tt=1"><button className="btn me-2" type="button" id="daroidi"><img src="../../public/image/icon/null.png" alt className="img-icon" />Đã rời
+                                            đi</button></a>
+                                        <input type="text" className="btn me-2" placeholder="Tìm sinh viên" id="find_MP" />
+                                        <select id="find_column" className="btn me-2">
+                                            <option value="#">#</option>
+                                            <option value="phong">Phòng</option>
+                                            <option value="ten">Tên</option>
+                                            <option value="ngaysinh">Ngày sinh</option>
+                                            <option value="Ngày vào">Ngày vào</option>
+                                        </select>
+                                    </form>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="table-sv">
+                            <table className="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Phòng</th>
+                                        <th scope="col">Tên</th>
+                                        <th scope="col">Ngày sinh</th>
+                                        <th scope="col">Ngày vào</th>
+                                        <th scope="col">Trạng thái</th>
+                                        <th scope="col">Hành động</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="body_table">
+                                </tbody>
+                            </table>
+                        </div>
+                        <nav aria-label="...">
+                            <ul className="pagination pagination-sm">
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default danhsachsv;
